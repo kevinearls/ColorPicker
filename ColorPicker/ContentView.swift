cf4f0c89afea2467aeed3c3e9f6a28802cd76a55
Kevin Earls <kevinearls@mac.com>
2023-12-03 14:29:04 +0100

    Initial working version
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -8,15 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var color:Color = .red
+  @State private var redValue = 0.0;
+  @State private var greenValue = 0.0;
+  @State private var blueValue = 0.0;
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          VStack {
+            Text("Color Picker")
+              .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            RoundedRectangle(cornerSize: CGSize(width: 0.0, height: 0.0), style: .continuous)
+              .foregroundColor(color)
+            Text("Red")
+            HStack {
+              Slider(value: $redValue, in: 0.0...255.0)
+              Text("\(Int(redValue.rounded()))")
+            }
+            Text("Green")
+            HStack {
+              Slider(value: $greenValue, in: 0.0...255.0)
+              Text("\(Int(greenValue.rounded()))")
+            }
+            Text("Blue")
+            HStack {
+              Slider(value: $blueValue, in: 0.0...255.0)
+              Text("\(Int(blueValue.rounded()))")
+            }
+            Button("Set Color") {
+              calculateAndSetColor()
+            }
+
+
+          }
         }
         .padding()
     }
+
+  func calculateAndSetColor() {
+    let redComponent = redValue / 255
+    let blueComponent = blueValue / 255
+    let greenComponent = greenValue / 255
+
+
+    color = Color(red: redComponent, green: greenComponent, blue: blueComponent)
+  }
 }
 
 #Preview {
