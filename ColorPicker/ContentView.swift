f12c054c4fa696710fa82d7cc41394540c4fb89a
Kevin Earls <kevinearls@mac.com>
2023-12-03 14:47:41 +0100

    Minor reformatting
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -8,53 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var color:Color = .red
   @State private var redValue = 0.0;
-  @State private var greenValue = 0.0;
-  @State private var blueValue = 0.0;
+  @State private var greenValue = 128.0;
+  @State private var blueValue = 255.0;
+  @State private var color = Color(red: 0.0, green: 128.0, blue: 255.0)
 
-    var body: some View {
-        VStack {
-          VStack {
-            Text("Color Picker")
-              .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            RoundedRectangle(cornerSize: CGSize(width: 0.0, height: 0.0), style: .continuous)
-              .foregroundColor(color)
-            Text("Red")
-            HStack {
-              Slider(value: $redValue, in: 0.0...255.0)
-              Text("\(Int(redValue.rounded()))")
-            }
-            Text("Green")
-            HStack {
-              Slider(value: $greenValue, in: 0.0...255.0)
-              Text("\(Int(greenValue.rounded()))")
-            }
-            Text("Blue")
-            HStack {
-              Slider(value: $blueValue, in: 0.0...255.0)
-              Text("\(Int(blueValue.rounded()))")
-            }
-            Button("Set Color") {
-              calculateAndSetColor()
-            }
+  var colorRange = 0.0...255.0
 
-
-          }
+  var body: some View {
+    VStack {
+      VStack {
+        Text("Color Picker")
+          .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerSize: CGSize(width: 0.0, height: 0.0), style: .continuous)
+          .foregroundColor(color)
+        Text("Red")
+        HStack {
+          Slider(value: $redValue, in: colorRange)
+          Text("\(Int(redValue.rounded()))")
         }
-        .padding()
+        Text("Green")
+        HStack {
+          Slider(value: $greenValue, in: colorRange)
+          Text("\(Int(greenValue.rounded()))")
+        }
+        Text("Blue")
+        HStack {
+          Slider(value: $blueValue, in: colorRange)
+          Text("\(Int(blueValue.rounded()))")
+        }
+        Button("Set Color") {
+          calculateAndSetColor()
+        }
+      }
     }
+    .padding()
+  }
 
   func calculateAndSetColor() {
     let redComponent = redValue / 255
     let blueComponent = blueValue / 255
     let greenComponent = greenValue / 255
 
-
     color = Color(red: redComponent, green: greenComponent, blue: blueComponent)
   }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
