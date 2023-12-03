cfce43d76af7a62b8fb263c0d90db707192aef6f
Kevin Earls <kevinearls@mac.com>
2023-12-03 12:33:03 +0100

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Kevin Earls on 03/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
